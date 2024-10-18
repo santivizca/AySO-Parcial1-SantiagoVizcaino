a2b258adcfeab9ea0dd4729f29c6291e6f9cf749
santivizca <santivizca20@gmail.com>
2024-10-18 00:54:31 +0000

    fin ejercicio c

---
+++ puntoc.sh/puntoc.sh
@@ -0,0 +1,13 @@
+sudo useradd developer1
+sudo useradd tester1
+sudo useradd devops1
+sudo useradd devops2
+sudo groupadd grupodevops
+sudo groupadd grupodevelopers
+sudo groupadd grupotesters
+sudo usermod -aG grupodevelopers developer1
+sudo usermod -aG grupodevops developer1
+sudo usermod -aG grupotesters tester1
+sudo usermod -aG grupodevops devops1
+sudo usermod -aG grupodevelopers devops1
+sudo usermod -aG grupodevops devops2
