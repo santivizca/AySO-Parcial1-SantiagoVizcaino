a5cf4b9d3e91a74b4c29b043a1b27ea4fe18d773
santivizca <santivizca20@gmail.com>
2024-10-18 01:46:10 +0000

    fin ejercicio F

---
+++ puntob.sh/puntob.sh
@@ -0,0 +1,18 @@
+lsblk
+ls /dev
+sudo fdisk /dev/sdc
+lsblk
+ls /dev
+sudo mkfs.ext4 /dev/sdc2
+sudo mkfs.ext4 /dev/sdc1
+sudo mkfs.ext4 /dev/sdc3
+sudo mkfs.ext4 /dev/sdc4
+sudo mkdir /mmt/sdc1
+sudo mkdir /mnt/sdc1
+sudo mount /dev/sdc1 /mnt/sdc1/
+sudo mkdir /mnt/sdc2
+sudo mkdir /mnt/sdc3
+sudo mkdir /mnt/sdc4
+sudo mount /dev/sdc2 /mnt/sdc2/
+sudo mount /dev/sdc3 /mnt/sdc3/
+
